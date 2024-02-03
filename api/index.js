e4b9dc49a2c06bb07654c91a278fa940376bc14e
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-02-03 22:12:31 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv'); 
+dotenv.config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to MongoDB!');
+})
+.catch((err) => {
+  console.log(err);
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
+
