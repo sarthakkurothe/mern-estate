cd8a4fc0f30194817199272e07e5ef7f940902db
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-03-06 19:38:36 +0530

    add signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -68,3 +68,12 @@ exports.google = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
+  } catch (error) {
+    next(error);
+  }
+};
