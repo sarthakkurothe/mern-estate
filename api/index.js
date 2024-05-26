dc4dee9c9e836d99a2c715dfb172cd3758865116
sarthakkurothe <reachsarthakkurothe@gmail.com>
2024-05-26 14:12:37 +0530

    changes in deployment

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRouter = require('./routes/auth.route.js');
 const listingRouter = require('./routes/listing.route.js');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+
 dotenv.config();
 
 mongoose
@@ -17,33 +18,31 @@ mongoose
     console.log(err);
   });
 
-  const __dirname = path.resolve();
-
 const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
+});
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
-
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
+  res.status(statusCode).json({
     success: false,
     statusCode,
     message,
